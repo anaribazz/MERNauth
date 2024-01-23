7adf79eef87f59515973df48e72b27ded8ec6d00
Ana Ribas <anaribas@proton.me>
2024-01-23 01:58:07 -0800

    update profile controller added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -10,7 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
   // throw new Error('Something went wrong');
   // res.status(200).json({ message: "Auth User" });
   const { email, password } = req.body;
-  const user = await User.findOne( {email} );
+  const user = await User.findOne( { email } );
 
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
@@ -32,8 +32,6 @@ const authUser = asyncHandler(async (req, res) => {
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-  console.log(name);
-
   const userExists = await User.findOne({ email: email }); //or just email
 
   if (userExists) {
@@ -43,7 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    password,
+    password
   });
 
   if (user) {
@@ -81,7 +79,13 @@ const logoutUser = asyncHandler(async (req, res) => {
 const userProfile = asyncHandler(async (req, res) => {
   // res.status(401);
   // throw new Error('Something went wrong');
-  res.status(200).json({ message: "User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  }
+
+  res.status(200).json(user);
 });
 
 //@description     Update user profile
@@ -90,6 +94,31 @@ const userProfile = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
   // res.status(401);
   // throw new Error('Something went wrong');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    
+   user.name == req.body.name || user.name;
+   user.email == req.body.email || user.email;
+
+   if (user.req.body.password) {
+    user.password == req.body.password;
+   }
+
+   const updatedUser = await user.save();
+
+   res.status(200).json({
+    _id: updatedUser._id,
+    name: updatedUser.name,
+    email: updatedUser.email
+
+   })
+
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
   res.status(200).json({ message: "Update User Profile" });
 });
 export { authUser, registerUser, logoutUser, userProfile, updateUserProfile };
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,19 +1,18 @@
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 
 const generateToken = (res, userId) => {
-    const token = jwt.sign({ userId }, `${process.env.JWT_SECRET_KEY}` , {
-        expiresIn: "1h" // 1 hour
+    const token = jwt.sign({ userId }, `${process.env.JWT_SECRET}`, {
+        expiresIn: "30d" // 3o days
     });
     // put in a cookie
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: `${process.env.NODE_ENV}` !== 'development',
         sameSite:'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-
+        maxAge: 30 * 24 * 60 * 60 * 1000
     });
 };
 
